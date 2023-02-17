74adf014fb1b5310ba4f74a75ddf60a96b97381a
MarcoAlcazar <112454804+MarcoAlcazar@users.noreply.github.com>
2023-02-17 00:42:27 -0800

    updates

--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'myapp.apps.myappConfig'
+    'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
