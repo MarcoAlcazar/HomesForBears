ff7e9957fbb244b8af5f126729eb1aed557ef0ed
MarcoAlcazar <112454804+MarcoAlcazar@users.noreply.github.com>
2023-02-17 00:23:48 -0800

    updates

--- myproj/myapp/models.py
+++ myproj/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Landlord(models.Model):
+    First_Name= models.CharField(max_length = 25)
+    Last_Name = models.CharField(max_length = 25)
+    Rating = models.IntegerField()
+    Description = models.CharField(max_length = 250)
+
+class Housing(models.Model):
+    Address = models.CharField(max_length=50)
+    Description = models.CharField(max_length = 250)
+    Rating = models.IntegerField()
+    price = models.IntegerField()
+
+
