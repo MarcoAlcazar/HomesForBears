d2b8119e3a22996b1411f027e41838f704bf602c
MarcoAlcazar <112454804+MarcoAlcazar@users.noreply.github.com>
2023-02-17 03:07:47 -0800

    updating

--- myproj/myapp/models.py
+++ myproj/myapp/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class Landlord(models.Model):
-    First_Name= models.CharField(max_length = 25)
-    Last_Name = models.CharField(max_length = 25)
-    Rating = models.IntegerField()
-    Description = models.CharField(max_length = 250)
-    
-
 class Housing(models.Model):
     Address = models.CharField(max_length=50)
+
+class HousingReview(models.Model):
+    Address = models.ForeignKey(Housing, on_delete= models.CASCADE)
     Description = models.CharField(max_length = 250)
     Rating = models.IntegerField()
     price = models.IntegerField()
-    LandLord = models.CharField(max_length = 200)
+    LandLord = models.CharField(max_length = 200, default = "none")
 
+
+class Landlord(models.Model):
+    FullName= models.CharField(max_length = 60, default = "none")
+    
+class LandlordReview(models.Model):
+    Landlord = models.ForeignKey(Landlord, on_delete= models.CASCADE)
+    Rating = models.IntegerField()
+    Description = models.CharField(max_length = 250)
+    
 
--- myproj/myapp/views.py
+++ myproj/myapp/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+def detail(request, landlords):
+    return HttpResponse("Youre looking at %s." % landlords)
+
+def detail(request, apartment):
+    return HttpResponse("Youre looking at %s." % apartment)
+
