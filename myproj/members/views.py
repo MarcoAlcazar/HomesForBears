ceddd53e034a98f5e39c46b8ebdcb8706a9d555f
Marco Alcazar <marcoca32@berkeley.edu>
2023-10-16 15:00:25 -0700

    comitting

--- myproj/members/views.py
+++ myproj/members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
+from django.contrib.auth.forms import UserCreationForm
+
 
 def login_user(request): 
     if request.method == "POST":
@@ -21,5 +23,20 @@ def logout_user(request) :
     logout(request)
     messages.success(request, ("You Have Been Logged Out!"))
     return redirect('index')
+
+def register_user(request) :
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, ("You Are Now Registered!"))
+            return redirect('index')
+    else: 
+        form  = UserCreationForm()
+    return render(request, 'authenticate/register_user.html', {'form':form})
 
 # Create your views here.
