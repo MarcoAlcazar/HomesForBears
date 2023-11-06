d849351c10810e9f80cee52f9046ad70d41da78a
MarcoAlcazar <112454804+MarcoAlcazar@users.noreply.github.com>
2023-11-05 19:12:51 -0800

    ummm

--- myproj/members/urls.py
+++ myproj/members/urls.py
@@ -11,5 +11,15 @@ urlpatterns = [
    path('logout_user', views.logout_user, name = 'logout'),
    path('register_user', views.register_user, name = 'register_user'),
    path('edit_profile/', UserEditView.as_view(), name = 'edit_profile'),
-   
+   path("reset_password/", auth_views.PasswordResetView.as_view(
+        template_name = "authenticate/password_reset.html"), name = "reset_password"),
+    
+   path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(
+        template_name = "authenticate/password_reset_sent.html"), name="password_reset_done"),
+    
+   path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name = "authenticate/password_reset_form.html"), name = "password_reset_confirm"),
+    
+   path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(
+        template_name = "authenticate/password_reset_done.html"), name = "password_reset_complete"),
 ]
--- myproj/myapp/urls.py
+++ myproj/myapp/urls.py
@@ -34,16 +34,4 @@ urlpatterns = [
     path('search_apartments', views.search_apartments, name = 'search-apartments' ),
     
     path('password/change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-   
-    path("reset_password/", auth_views.PasswordResetView.as_view(
-        template_name = "authenticate/password_reset.html"), name = "reset_password"),
-    
-    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(
-        template_name="templates/authenticate/password_reset_sent.html"), name="password_reset_done"),
-    
-    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
-        template_name = "authenticate/password_reset_form.html"), name = "password_reset_confirm"),
-    
-    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(
-        template_name = "authenticate/password_reset_done.html"), name = "password_reset_complete"),
 ]
