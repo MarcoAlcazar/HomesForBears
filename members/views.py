5e9858051c2c04e75cea84fc463531dae69b63c1
Marco Alcazar <marcoca32@berkeley.edu>
2023-11-09 01:37:12 -0800

    hmm

--- members/views.py
+++ members/views.py
@@ -10,7 +10,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str 
 from django.urls import reverse_lazy
 from django.core.mail import EmailMessage
-from .token import account_activation_token
+from .tokens.py import account_activation_token
 
 class UserEditView(generic.UpdateView):
     form_class = EditProfileForm
