a8aa0531c5bd5199cc18bacc272125a1074e1e9f
MarcoAlcazar <112454804+MarcoAlcazar@users.noreply.github.com>
2024-01-03 18:43:53 +0100

    save

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 from PIL import Image 
+import io
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # Create your models here.
 def resize_image(image, max_width=500, max_height=500):
@@ -35,29 +37,32 @@ class Housing(models.Model):
         # Define how you want the object to be displayed
         return self.Address
 
-    #def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+        self.resize_images()
+
+    def resize_images(self):
+        # Sample implementation using PIL to resize images
         if self.housing_image:
-            resize_image(self.housing_image)
+            self.resize_image(self.housing_image)
         if self.housing_image2:
-            resize_image(self.housing_image2)
+            self.resize_image(self.housing_image2)
         if self.housing_image3:
-            resize_image(self.housing_image3)
+            self.resize_image(self.housing_image3)
         if self.housing_image4:
-            resize_image(self.housing_image4)
+            self.resize_image(self.housing_image4)
         if self.housing_image5:
-            resize_image(self.housing_image5)
+            self.resize_image(self.housing_image5)
 
-
-
-class Landlord(models.Model):
-    FullName= models.CharField(max_length = 60, default = "")
-    Rating = models.IntegerField(default = "", validators=[MinValueValidator(1), MaxValueValidator(5)])
-    Description = models.CharField(max_length = 250, default = "")
-
-    def __str__(self):
-        # Define how you want the object to be displayed
-        return self.FullName
+    def resize_image(self, image_field):
+        # Sample implementation using PIL to resize an image
+        image = Image.open(image_field)
+        output = io.BytesIO()
+        image = image.resize((300, 300))  # Adjust the size as needed
+        image.save(output, format='JPEG', quality=90)
+        output.seek(0)
+        image_field.file = InMemoryUploadedFile(output, 'ImageField', f"{image_field.name.split('.')[0]}_resized.jpg",
+                                                'image/jpeg', output.tell(), None)
 
 
 
