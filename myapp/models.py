ca4bd04cac5a330fb4e087615281452449fde296
MarcoAlcazar <112454804+MarcoAlcazar@users.noreply.github.com>
2024-01-03 18:52:49 +0100

    save

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
-from PIL import Image 
-import io
+from PIL import Image
+import json
+import boto3
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from botocore.exceptions import NoCredentialsError
 
 # Create your models here.
 def resize_image(image, max_width=500, max_height=500):
@@ -18,20 +19,23 @@ def resize_image(image, max_width=500, max_height=500):
     
     img.save(image.path)
 
+def upload_to_s3(data, filename, folder):
+    """
+    Upload data to an S3 bucket.
+    """
+    S3_BUCKET = 'homesforbears'
+
+    # Initialize the S3 client
+    s3 = boto3.client('s3', aws_access_key_id='AKIAW3TQQ24DWM5MXPNB', aws_secret_access_key='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')
+
+    try:
+        # Upload data to S3
+        s3.put_object(Body=data, Bucket=S3_BUCKET, Key=f'{folder}/{filename}')
+    except NoCredentialsError:
+        print("Credentials not available")
+
 class Housing(models.Model):
     # ... Your other model fields ...
-    Address = models.CharField(max_length=50)
-    Bedrooms = models.IntegerField(default="", validators=[MinValueValidator(1), MaxValueValidator(25)])
-    Bathrooms = models.IntegerField(default="", validators=[MinValueValidator(1), MaxValueValidator(25)])
-    Description = models.CharField(max_length=250, default="")
-    Rating = models.IntegerField(default="", validators=[MinValueValidator(1), MaxValueValidator(5)])
-    Price = models.IntegerField(default="", validators=[MinValueValidator(500), MaxValueValidator(50000)])
-    LandLord = models.CharField(max_length=200, default="")
-    housing_image = models.ImageField(null=True, blank=True, upload_to="images/")
-    housing_image2 = models.ImageField(null=True, blank=True, upload_to="images/")
-    housing_image3 = models.ImageField(null=True, blank=True, upload_to="images/")
-    housing_image4 = models.ImageField(null=True, blank=True, upload_to="images/")
-    housing_image5 = models.ImageField(null=True, blank=True, upload_to="images/")
 
     def __str__(self):
         # Define how you want the object to be displayed
@@ -40,6 +44,32 @@ class Housing(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         self.resize_images()
+
+        # Convert housing data to JSON format
+        housing_data = {
+            'address': self.Address,
+            'bedrooms': self.Bedrooms,
+            'bathrooms': self.Bathrooms,
+            'description': self.Description,
+            'rating': self.Rating,
+            'price': self.Price,
+            'landlord': self.LandLord,
+        }
+        housing_data_json = json.dumps(housing_data)
+
+        # Upload housing data to S3
+        upload_to_s3(housing_data_json, f'{self.id}_housing.json', 'housing')
+
+        # Upload landlord data to S3
+        landlord_data = {
+            'full_name': self.LandLord.FullName,
+            'rating': self.LandLord.Rating,
+            'description': self.LandLord.Description,
+        }
+        landlord_data_json = json.dumps(landlord_data)
+
+        # Upload landlord data to S3
+        upload_to_s3(landlord_data_json, f'{self.id}_landlord.json', 'landlord')
 
     def resize_images(self):
         # Sample implementation using PIL to resize images
@@ -62,7 +92,18 @@ class Housing(models.Model):
         image.save(output, format='JPEG', quality=90)
         output.seek(0)
         image_field.file = InMemoryUploadedFile(output, 'ImageField', f"{image_field.name.split('.')[0]}_resized.jpg",
-                                        'image/jpeg', output.tell(), None)
+                                                'image/jpeg', output.tell(), None)
+
+class Landlord(models.Model):
+    FullName = models.CharField(max_length=60, default="")
+    Rating = models.IntegerField(default="", validators=[MinValueValidator(1), MaxValueValidator(5)])
+    Description = models.CharField(max_length=250, default="")
+
+    def __str__(self):
+        # Define how you want the object to be displayed
+        return self.FullName
+
+
 
 
 
